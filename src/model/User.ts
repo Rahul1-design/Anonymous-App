dbc5e00d244f8090e9efc2728f04d7f03af9ab40
Rahul Shrestha <rahulshrestha3132@gmail.com>
2025-11-28 16:46:22 +0545

    Initial commit: Add user model and validation schemas

---
+++ src/model/User.ts
@@ -0,0 +1,76 @@
+import { Schema, Document, Mongoose } from "mongoose";
+import mongoose from "mongoose";
+
+export interface Message extends Document{
+    content: string;
+    createdAt: Date
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+export interface User extends Document{
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[]
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        true: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true,"Email is required"],
+        unique: true,
+        match: [/.+\@.+\.+/,"Please use a valid email address"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify code is required"],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify code Expiry is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true,
+    },
+    messages: [MessageSchema]
+})
+
+
+//  In Next.js development, when you save a file, the code reloads. Without this check, mongoose would try to create the same model multiple times and throw an error: "Cannot overwrite 'User' model"
+
+// what it does 
+// Check if the User Model exit and if it does'nt create one
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel;
+
