b40eac9368b4a538ac86e2c812ae13e23c4d5b4f
Rahul Shrestha <rahulshrestha3132@gmail.com>
2025-12-08 17:07:02 +0545

    React hook form, shadcn and debouncing

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,26 +1,8 @@
-"use client"
-
-import { useDebounceValue } from 'usehooks-ts'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z  from "zod"
-import Link from "next/link"
-import { useState } from "react"
-import { toast } from "sonner"
-import { useRouter } from 'next/navigation'
+import React from 'react'
 
 const page = () => {
-    const [username, setUsername] = useState('');
-    const [usernameMessage, setUsernamemessage] = useState('')
-    const [isCheckingUsername, setIsCheckingUsername] = useState(false)
-    const [isSubmitting, setIsSubmitting] = useState(false)
-    const debouncedUsername = useDebounceValue(username, 300)
-    const router = useRouter()
-
-    // zod Implementation
-
   return (
-    <div>page</div>
+    <div>Sign In Page</div>
   )
 }
 
